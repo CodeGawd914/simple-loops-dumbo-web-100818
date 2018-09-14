7b224b6df151ff944091fe882a7b1791795f3975
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-09-14 15:59:34 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,8 @@
 
 def loop_iterator(number_of_times)
   loop do
-   puts phrase
-    break if phrase == number_of_times 
+   puts number_of_times
+    break if phrase == number_of_times
   # Code your solution here using the "loop" keyword to puts out the below phras
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
