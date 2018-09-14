0a1d3dbb4d48f085fb93d2259b71ff69ca7876b3
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-09-14 16:02:38 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,7 @@
 
 def loop_iterator(number_of_times)
   loop do
-   puts 
+   phrase
     break if phrase == number_of_times
   # Code your solution here using the "loop" keyword to puts out the below phras
   # This and all the other methods take an argument of an integer 
