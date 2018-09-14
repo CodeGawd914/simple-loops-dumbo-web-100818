7f73583df565a5a00ebd1d5e371c094b67595046
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-09-14 16:01:10 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,6 +2,7 @@
 
 def loop_iterator(number_of_times)
   loop do
+    phrase = "Welcome to Flatiron School's Web Development Course!"
    puts phrase
     break if phrase == number_of_times
   # Code your solution here using the "loop" keyword to puts out the below phras
